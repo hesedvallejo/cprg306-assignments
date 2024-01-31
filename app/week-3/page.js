a0894f4ae6e73e02683077002a6cf49861029b51
hesedvallejo <hesed.vallejo@edu.sait.ca>
2024-01-31 09:27:04 -0700

    week 3

---
+++ app/week-3/page.js
@@ -0,0 +1,13 @@
+import ItemList from "./item-list";
+
+export default function Page() {
+  return (
+    <main>
+      <h1>
+        Shopping List
+      </h1>
+      <ItemList />
+     
+    </main>
+  );
+}
